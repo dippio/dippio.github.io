813faaeb4bb2a9ad70594e4b9db7ea3635e1e2d0
tyler <111218991+dippio@users.noreply.github.com>
2023-09-25 00:55:38 +1000

    new temporary page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-"" 
+
+// i dont really do js, so this is broken as hell
+function themeToggle() {
+    let currentTheme = "dark";
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    }
