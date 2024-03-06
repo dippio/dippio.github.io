d177fb9cc2c3f781bcfe76f4e5b9e4b305e60c0f
tyler <tylerbatkin@gmail.com>
2024-03-06 23:45:42 +1000

    fix post listing page

---
+++ js/postlist.js
@@ -0,0 +1,48 @@
+fetch('./posts.json')
+.then(response => response.json())
+.then(data => {
+    const postsTableBody = document.querySelector('#posts tbody');
+
+    // Sort the posts by date in descending order
+    data.sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateB - dateA;
+    });
+
+    recentPosts.forEach(post => {
+        const row = postsTableBody.insertRow();
+        const dateCell = row.insertCell(0);
+        const titleCell = row.insertCell(1);
+        
+        dateCell.textContent = post.date;
+        dateCell.style.textAlign = "left";
+        
+        // Set title text and make it clickable with link
+        const titleLink = document.createElement('a');
+        titleLink.href = post.linksto;
+        titleLink.textContent = post.title;
+        titleCell.appendChild(titleLink);
+        titleCell.style.textAlign = "right";
+    });
+})
+.catch(error => {
+    console.error('Error fetching posts data: ', error);
+});
+
+// get screen aspect ratio
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var windowHeight = window.innerHeight;
+    var windowWidth = window.innerWidth;
+    
+    var aspectRatio = windowWidth / windowHeight;
+
+    if (aspectRatio > 1.4) {
+        document.body.style.width = "30%";
+    } else if (aspectRatio >= 1 && aspectRatio <= 1.4) {
+        document.body.style.width = "40%";
+    } else {
+        document.body.style.width = "85%";
+    }
+});
