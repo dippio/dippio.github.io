cf793fe5ac10c4d1afa5157aaba265efa0ff8b98
tyler <111218991+dippio@users.noreply.github.com>
2024-05-14 23:41:22 +1000

    change neko location

--- js/main.js
+++ js/main.js
@@ -6,56 +6,18 @@ document.addEventListener("DOMContentLoaded", function() {
     
     var aspectRatio = windowWidth / windowHeight;
 
+    var mainContentBox = document.querySelector('.mainContentBox');
+
     if (aspectRatio > 1.4) {
         mainContentBox.style.width = "23vw";
+        console.log("24")
     } else if (aspectRatio >= 1 && aspectRatio <= 1.4) {
-        mainContentBox.style.width = "40vw";
+        mainContentBox.style.width = "35vw";
     } else {
-        mainContentBox.style.width = "85vw";
+        mainContentBox.style.width = "80vw";
+        mainContentBox.style.height = "80vh";
     }
 });
-
-// post loader
-fetch('../posts.json')
-.then(response => response.json())
-    .then(data => {
-        document.addEventListener("DOMContentLoaded", function() {
-            const postsTableBody = document.querySelector('#posts tbody');
-
-            // sort by date
-            data.sort((a, b) => {
-                const dateA = new Date(a.date);
-                const dateB = new Date(b.date);
-                return dateB - dateA;
-            });
-
-            // only 3 posts for home page
-            if (document.title === "tyler batkin // welcome" || "tylerbatk.in // welcome") { 
-                data = data.slice(0, 3);
-            }
-
-            data.forEach(post => {
-                const row = postsTableBody.insertRow();
-                const dateCell = row.insertCell(0);
-                const titleCell = row.insertCell(1);
-                
-                dateCell.textContent = post.date;
-                dateCell.style.textAlign = "left";
-                
-                // Set title text and make it clickable with link
-                const titleLink = document.createElement('a');
-                titleLink.href = post.linksto;
-                titleLink.textContent = post.title;
-                titleCell.appendChild(titleLink);
-                titleCell.style.textAlign = "right";
-            });
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching posts data: ', error);
-    });
-
-
 
 
 // random flavor text for main page
@@ -74,25 +36,3 @@ document.addEventListener("DOMContentLoaded", function() {
     var flavorTextElement = document.getElementById("flavorText");
     flavorTextElement.innerHTML = "<i>" + flavorText + "</i>";
 });
-
-
-
-
-// date stuff for posts
-if (window.location.pathname.includes("/posts/")) {
-    // format as dd / mm / yy
-    function formatDate(date) {
-        var day = date.getDate();
-        var month = date.getMonth() + 1;
-        var year = date.getFullYear().toString().substr(-2); // Get last two digits of the year
-        return (day < 10 ? '0' : '') + day + ' / ' + (month < 10 ? '0' : '') + month + ' / ' + year;
-    }
-
-    var modificationDate = new Date(document.lastModified);
-
-    var formattedModificationDate = formatDate(modificationDate);
-
-    document.addEventListener("DOMContentLoaded", function() {
-        document.getElementById("modificationDate").textContent = formattedModificationDate;
-    });
-}
